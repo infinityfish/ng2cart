cc0e2105fcbb1d6b37b8476d79c4ad633af64757
ericmabo <infinityfish360@gmail.com>
2016-05-25 08:24:34 -0400

    first commit

--- src/app/ng2cart.component.ts
+++ src/app/ng2cart.component.ts
@@ -7,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['ng2cart.component.css']
 })
 export class Ng2cartAppComponent {
-  title = 'ng2cart works!';
+  title = 'ng2cart';
+  public newItem = "";
+  public cart = [];   
+	public products = [
+		{name:"Brakes for Toyota Corrolla", qty: 10, price: 9.95},
+    {name:"Clutches", qty: 4, price: 129.95},
+    {name:"Suspension", qty: 2, price: 19.95},
+    {name:"Plugs", qty: 20, price: 3.95}
+	];
+  
+  
+  // constructor(public myTotal = 0){}
+  
+  	addToCart(product){
+	   
+	    this.newItem = product;
+	    this.cart.push(product);
+	    
+	    alert("Item added to cart: " + product.name);
+	}
+  
+  cartTotal(){     
+      this.cart.forEach(function(item,index){
+        console.log(item.qty * item.price); 
+        // this.cartTotal +=   item.qty * item.price;
+        //  console.log(this.cartTotal);
+      }); 
+      
+     
+         
+  }
+  
+  removeItem = function(index) {
+        this.cart.splice(index, 1);
+    }
+  
+
+	            
 }
